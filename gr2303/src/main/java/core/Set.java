e17fffa590779256884a6b02e60436afa0a04bfd
Martin <Martirev@stud.ntnu.no>
2023-09-14 18:13:55 +0200

    Made classes Workout,Exercise,Set
    Made the classes for the logic for the workout

---
+++ gr2303/src/main/java/core/Set.java
@@ -0,0 +1,21 @@
+package core;
+
+public class Set {
+
+    private int reps;
+    private int weight;
+
+    public Set(int reps, int weight) {
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
