e14b28ffe642dd8454b435ec0dcc1a5d89d519bb
2Gomaskin <2GoMaskin@2Go.no>
2023-09-16 15:19:04 +0200

    Started on the ReadAndWrite class, added one dependency and alterd the module-info file

---
+++ gr2303/src/main/java/core/ReadAndWrite.java
@@ -0,0 +1,17 @@
+package core;
+
+//imports
+import org.json.simple.JSONObject;
+
+public class ReadAndWrite {
+
+    
+
+    JSONObject user = new JSONObject();
+    JSONObject workout = new JSONObject();
+    JSONObject exercise = new JSONObject();
+    JSONObject set = new JSONObject();
+    
+    user.put("name", )
+    
+}
--- gr2303/src/main/java/module-info.java
+++ gr2303/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ui {
     requires javafx.controls;
     requires javafx.fxml;
+    //Json
+    requires json.simple;
 
     opens ui.controllers to javafx.graphics, javafx.fxml;
     opens ui to javafx.graphics, javafx.fxml;
