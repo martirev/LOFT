ff21ee013b10a27fb0b5f4a9ba39d33332d46cd8
Markus Helbæk <markhelb@stud.ntnu.no>
2023-09-18 12:25:31 +0200

    Tried to remove some of the Type-safty warnings, perhaps the json library is not the best

--- gr2303/src/main/java/core/ReadAndWrite.java
+++ gr2303/src/main/java/core/ReadAndWrite.java
@@ -11,27 +11,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadAndWrite {
     
+    //Dummy name for the user, the implementation of the user class is made with the assumption that there is only one user.
     private static String dummyName = "dummyName";
 
     //Private read attributes
     private JSONParser parser = new JSONParser();
-
     private Collection<User> userClasses = new ArrayList<User>();
     private Collection<Workout> workoutsClasses = new ArrayList<Workout>();
     private Collection<Exercise> exercisesClasses = new ArrayList<Exercise>();
     private Collection<Set> setsClasses = new ArrayList<Set>();
     
-    //write methods
-    private JSONArray setSetsJsonArray(List<Set> list) {
-        JSONArray setsArray = new JSONArray();
+    //Private write methods
+    private ArrayList<HashMap<String, Object>> setSetsJsonArray(List<Set> list) {
+        ArrayList<HashMap<String, Object>> setsArray = new ArrayList<HashMap<String, Object>>();
         int i = 0;
         for (Set set : list) {
-            JSONObject setJson = new JSONObject();
+            HashMap<String, Object> setJson = new HashMap<String, Object>();
             setJson.put("setNumber", ++i);
             setJson.put("weight", set.getWeight());
             setJson.put("reps", set.getReps());
@@ -39,53 +40,50 @@ public class ReadAndWrite {
         }
         return setsArray;
     }
-
-    private JSONArray setExercisesJsonArray(List<Exercise> list) {
-        JSONArray exercisesArray = new JSONArray();
+    private ArrayList<HashMap<String, Object>> setExercisesJsonArray(List<Exercise> list) {
+        ArrayList<HashMap<String, Object>>  exercisesArray = new ArrayList<HashMap<String, Object>>();
         for (Exercise exercise : list) {
-            JSONObject exerciseJson = new JSONObject();
+            HashMap<String, Object> exerciseJson = new HashMap<String, Object>();
             exerciseJson.put("exerciseName", exercise.getName());
             exerciseJson.put("sets", setSetsJsonArray(exercise.getSets()));
-            exerciseJson.put("totalWeight", exercise.getTotalWeight());
-            exerciseJson.put("localPr", exercise.getLocalPr());
             exercisesArray.add(exerciseJson);
         }
         return exercisesArray;
     }
-
-    private JSONObject formatWorkoutClassToJson(Workout workout) {
-        JSONObject workoutJson = new JSONObject();
+    private HashMap<String, Object> formatWorkoutClassToJson(Workout workout) {
+        HashMap<String, Object> workoutJson = new HashMap<String, Object>();
         workoutJson.put("exercises", setExercisesJsonArray(workout.getExercises()));
-        workoutJson.put("totalWeight", workout.getTotalWeight());
         workoutJson.put("date", workout.getDate().toString());
         
         return workoutJson;
     }
 
-    //Writes the workout to the file, the code is made with the assumption that there is only one user,
-    //but it can be easily changed to support multiple users.
-    public void writeDataToFile(Workout workout) {
-        JSONArray exsistingData = readDataFromFile();
+    //Writing a workout class to the userData file in json format
+    public void writeWorkoutToUser(Workout workout) {
+        JSONArray existingData = readDataFromFile();
         JSONArray users = new JSONArray();
         
         JSONObject user = new JSONObject();
-
-        //Setting up user
-        if (exsistingData == null) {
+    
+        // Setting up user
+        if (existingData == null) {
             user.put("name", dummyName);
             user.put("workouts", new JSONArray());
         } else {
-            JSONObject userDummy = (JSONObject) exsistingData.get(0);
+            JSONObject userDummy = (JSONObject) existingData.get(0);
             user = userDummy;
         }
-
-        //Adding to user
-        JSONObject workoutInJSONFormat = this.formatWorkoutClassToJson(workout);
+    
+        // Adding to user
+        HashMap<String, Object> workoutInJSONFormat = this.formatWorkoutClassToJson(workout);
         JSONArray workoutsJsonArray = (JSONArray) user.get("workouts");
         workoutsJsonArray.add(workoutInJSONFormat);
-
-        users.add(user);
-
+    
+        users.add(user); 
+    /*  I am not sure how to remove these notifications. However the type safety, the way i see it, should be ok.
+     *  The type safty of the casts should be guarranteed in the way we are using the type Object.
+    */
+    
         try (FileWriter file = new FileWriter("gr2303/src/main/java/core/userData/userData.json")) {    
             file.write(users.toJSONString()); 
             file.flush();
@@ -93,7 +91,7 @@ public class ReadAndWrite {
             e.printStackTrace();
         }
     }
-
+    
     //private read methods
     private ArrayList<User> classReconstructor(JSONArray users) {
         for (Object userObject : users) {
@@ -119,9 +117,6 @@ public class ReadAndWrite {
                         Set newSet = new Set(weight, reps);
                         setsClasses.add(newSet);
                     }
-                    int exerciseLocalPr = ((Long) exercise.get("localPr")).intValue();
-                    int exerciseTotalWeight = ((Long) exercise.get("totalWeight")).intValue();
-
                     Exercise newExercise = new Exercise(exerciseName);
                     for (Set set : setsClasses) {
                         newExercise.addSet(set);
@@ -145,9 +140,13 @@ public class ReadAndWrite {
 
     private JSONArray readDataFromFile() {
         try (FileReader reader = new FileReader("gr2303/src/main/java/core/userData/userData.json")){
-            Object obj = parser.parse(reader);
-            JSONArray users = (JSONArray) obj;
-            return users;
+            if (reader.ready()) {
+                Object obj = parser.parse(reader);
+                JSONArray users = (JSONArray) obj;
+                return users;
+            } else {
+                //The file can be empty this is not an error, it just means that there is no data to read.
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -182,18 +181,16 @@ public class ReadAndWrite {
         exercise2.addSet(set2);
         workout2.addExercise(exercise2);
        
-        readAndWrite.writeDataToFile(workout1);
-        readAndWrite.writeDataToFile(workout2);
+        readAndWrite.writeWorkoutToUser(workout1);
+        readAndWrite.writeWorkoutToUser(workout2);
     //tests for readDataFromFile
         ReadAndWrite readAndWrite2 = new ReadAndWrite();
         User user2 = readAndWrite2.returnUserClassFromFile();
         System.out.println(user2.getName());
         System.out.println(user2.getNumberOfWorkouts());
-        System.out.println(user2.getWorkouts().get(0).getTotalWeight());
         System.out.println(user2.getWorkouts().get(0).getDate());
         System.out.println(user2.getWorkouts().get(0).getExercises().get(0).getName());
 
-        System.out.println(user2.getWorkouts().get(1).getTotalWeight());
         System.out.println(user2.getWorkouts().get(1).getDate());
         System.out.println(user2.getWorkouts().get(1).getExercises().get(0).getName());
 
