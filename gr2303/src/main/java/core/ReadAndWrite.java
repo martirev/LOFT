e88b4e4a2e7a5e5dfcec77b16761c3dc17f92f9a
Markus Helbæk <marken.helbk143@gmail.com>
2023-09-20 12:17:10 +0200

    Fixed the Type warnings.
    Had to refactor the ReadAndWrite class, added a UserAdaper class for reconstructing/deconstructing User class to/from Json. We are now using Gson. Minor changes on the pom.xml file and module-info file.

--- gr2303/src/main/java/core/ReadAndWrite.java
+++ gr2303/src/main/java/core/ReadAndWrite.java
@@ -1,215 +1,120 @@
 package core;
 
-//imports
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Reader;
+import java.io.Writer;
 
+/**
+ * This is a class for reading and writing to the loftUserData file stored in
+ * the users home directory.
+ * The two methods writeWorkoutToUser and returnUserClassFromFile are of most
+ * interest.
+ * The file is stored in json format. Using the UserAdapter class we can
+ * reconstruct classes form the
+ * json file, and write classes in json format.
+ * 
+ */
 public class ReadAndWrite {
-    
-    //Dummy name for the user, the implementation of the user class is made with the assumption that there is only one user.
-    private static String dummyName = "dummyName";
-
+    /**
+     * For the first version of the application we will only have one user. However,
+     * we have structured
+     * the code in a way that it is easy to add multiple users in the future.
+     * To parse the classes from/to json we are reconstructing/deconstructing the
+     * attributes through the
+     * UserAdapter class using the gson library.
+     */
     private static String fileFolderLocation = System.getProperty("user.home") + System.getProperty("file.separator");
-    
-    //Private read attributes
-    private JSONParser parser = new JSONParser();
-    private Collection<User> userClasses = new ArrayList<User>();
-    private Collection<Workout> workoutsClasses = new ArrayList<Workout>();
-    private Collection<Exercise> exercisesClasses = new ArrayList<Exercise>();
-    private Collection<Set> setsClasses = new ArrayList<Set>();
+    private UserAdapter userAdapter = new UserAdapter();
     private String fileLocation;
 
-    
+    /**
+     * The constructor for the ReadAndWrite class. It will use the default file
+     * location.
+     */
     public ReadAndWrite() {
         this(fileFolderLocation + "userData.json");
     }
-    
+
+    /**
+     * The constructor for the ReadAndWrite class. It will use the file location
+     * specified in the parameter.
+     * Used fofr the tests
+     * 
+     * @param location String
+     */
     public ReadAndWrite(String location) {
         this.fileLocation = location;
     }
-    
-    //Private write methods
-    private ArrayList<HashMap<String, Object>> setSetsJsonArray(List<Set> list) {
-        ArrayList<HashMap<String, Object>> setsArray = new ArrayList<HashMap<String, Object>>();
-        int i = 0;
-        for (Set set : list) {
-            HashMap<String, Object> setJson = new HashMap<String, Object>();
-            setJson.put("setNumber", ++i);
-            setJson.put("weight", set.getWeight());
-            setJson.put("reps", set.getReps());
-            setsArray.add(setJson);
-        }
-        return setsArray;
-    }
-    private ArrayList<HashMap<String, Object>> setExercisesJsonArray(List<Exercise> list) {
-        ArrayList<HashMap<String, Object>>  exercisesArray = new ArrayList<HashMap<String, Object>>();
-        for (Exercise exercise : list) {
-            HashMap<String, Object> exerciseJson = new HashMap<String, Object>();
-            exerciseJson.put("exerciseName", exercise.getName());
-            exerciseJson.put("sets", setSetsJsonArray(exercise.getSets()));
-            exercisesArray.add(exerciseJson);
-        }
-        return exercisesArray;
-    }
-    private HashMap<String, Object> formatWorkoutClassToJson(Workout workout) {
-        HashMap<String, Object> workoutJson = new HashMap<String, Object>();
-        workoutJson.put("exercises", setExercisesJsonArray(workout.getExercises()));
-        workoutJson.put("date", workout.getDate().toString());
-        
-        return workoutJson;
-    }
 
-    //Writing a workout class to the userData file in json format
+    /**
+     * The method to be used for writing a workout class to the userData file in
+     * json format.
+     * 
+     * @param workout
+     */
     public void writeWorkoutToUser(Workout workout) {
-        JSONArray existingData = readDataFromFile();
-        JSONArray users = new JSONArray();
-        
-        JSONObject user = new JSONObject();
-    
-        // Setting up user
+        User existingData = returnUserClassFromFile();
+        User user;
+
         if (existingData == null) {
-            user.put("name", dummyName);
-            user.put("workouts", new JSONArray());
+            user = new User();
         } else {
-            JSONObject userDummy = (JSONObject) existingData.get(0);
-            user = userDummy;
+            user = existingData;
         }
-    
-        // Adding to user
-        HashMap<String, Object> workoutInJSONFormat = this.formatWorkoutClassToJson(workout);
-        JSONArray workoutsJsonArray = (JSONArray) user.get("workouts");
-        workoutsJsonArray.add(workoutInJSONFormat);
-    
-        users.add(user); 
-    /*  I am not sure how to remove these notifications. However the type safety, the way i see it, should be ok.
-     *  The type safty of the casts should be guarranteed in the way we are using the type Object.
-    */
-    
-        try (FileWriter file = new FileWriter(fileLocation)) {    
-            file.write(users.toJSONString()); 
-            file.flush();
+        user.addWorkout(workout);
+
+        try (Writer file = new FileWriter(fileLocation)) {
+            JsonWriter jsonWriter = new JsonWriter(file);
+            userAdapter.write(jsonWriter, user);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    //private read methods
-    private ArrayList<User> classReconstructor(JSONArray users) {
-        for (Object userObject : users) {
-            JSONObject user = (JSONObject) userObject;
-            String name = (String) user.get("name");
-            
-            JSONArray workouts = (JSONArray) user.get("workouts");
-            for (Object workoutObject : workouts) {
-                JSONObject workout = (JSONObject) workoutObject;
-                LocalDate date = LocalDate.parse((String) workout.get("date"));
 
-                JSONArray exercises = (JSONArray) workout.get("exercises");
-                for (Object exerciseObject : exercises) {
-                    JSONObject exercise = (JSONObject) exerciseObject;
-                    String exerciseName = (String) exercise.get("exerciseName");
-
-                    JSONArray sets = (JSONArray) exercise.get("sets");
-                    for (Object setObject : sets) {
-                        JSONObject set = (JSONObject) setObject;
-                        int weight = ((Long) set.get("weight")).intValue();
-                        int reps = ((Long) set.get("reps")).intValue();
-
-                        Set newSet = new Set(weight, reps);
-                        setsClasses.add(newSet);
-                    }
-                    Exercise newExercise = new Exercise(exerciseName);
-                    for (Set set : setsClasses) {
-                        newExercise.addSet(set);
-                    }
-                    exercisesClasses.add(newExercise);
-                }
-                Workout newWorkout = new Workout(date);
-                for (Exercise exercise : exercisesClasses) {
-                    newWorkout.addExercise(exercise);
-                }
-                workoutsClasses.add(newWorkout);
-            }
-            User newUser = new User(name);
-            for (Workout workout : workoutsClasses) {
-                newUser.addWorkout(workout);
-            }
-            userClasses.add(newUser);
-        }
-        return (ArrayList<User>) userClasses;
-    }
-
-    private JSONArray readDataFromFile() {
-        try (FileReader reader = new FileReader(fileLocation)){
+    /**
+     * The method to be used for reading the userData file in json format.
+     * 
+     * @return User class
+     */
+    public User returnUserClassFromFile() {
+        try (Reader reader = new FileReader(fileLocation)) {
             if (reader.ready()) {
-                Object obj = parser.parse(reader);
-                JSONArray users = (JSONArray) obj;
-                return users;
+                JsonReader jsonReader = new JsonReader(reader);
+                User user = userAdapter.read(jsonReader);
+                return user;
             } else {
-                //The file can be empty this is not an error, it just means that there is no data to read.
+                return null;
             }
         } catch (FileNotFoundException e) {
-            // If it is not found, it will be created at a later point, so this is fine
-            return null;
+            // If it is not found, it will be created at a later point, so this is fine.
+            return new User();
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e){
             e.printStackTrace();
         }
         return null;
     }
 
-    //Returns the user class from the file
-    public User returnUserClassFromFile() {
-        JSONArray users = readDataFromFile();
-        if (users == null) {
-            return new User(dummyName);
-        }
-        ArrayList<User> user = this.classReconstructor(users);
-        User userClass = user.get(0);
-        return userClass;
-    }
+    public static void main(String[] args) {
+        // local tests
 
-    //quick test
-   public static void main(String[] args) {
-    //tests for writeDataToFile
         ReadAndWrite readAndWrite = new ReadAndWrite();
-        Workout workout1 = new Workout();
-        Exercise exercise1 = new Exercise("Bench Press");
-        Set set = new Set(100, 10);
-        Workout workout2 = new Workout();
-        Exercise exercise2 = new Exercise("Squat");
-        Set set2 = new Set(200, 10);
+        User user = new User();
+        Workout workout = new Workout();
+        Exercise exercise = new Exercise("test");
+        Set set1 = new Set(10, 10);
+        Set set2 = new Set(10, 10);
 
-        exercise1.addSet(set);
-        workout1.addExercise(exercise1);
-        exercise2.addSet(set2);
-        workout2.addExercise(exercise2);
-       
-        readAndWrite.writeWorkoutToUser(workout1);
-        readAndWrite.writeWorkoutToUser(workout2);
-    //tests for readDataFromFile
-        ReadAndWrite readAndWrite2 = new ReadAndWrite();
-        User user2 = readAndWrite2.returnUserClassFromFile();
-        System.out.println(user2.getName());
-        System.out.println(user2.getNumberOfWorkouts());
-        System.out.println(user2.getWorkouts().get(0).getDate());
-        System.out.println(user2.getWorkouts().get(0).getExercises().get(0).getName());
+        exercise.addSet(set1);
+        exercise.addSet(set2);
 
-        System.out.println(user2.getWorkouts().get(1).getDate());
-        System.out.println(user2.getWorkouts().get(1).getExercises().get(0).getName());
+        workout.addExercise(exercise);
+        user.addWorkout(workout);
 
-        System.out.println(user2.getWorkouts().get(0).getExercises().get(0).getSets().get(0).getWeight());
+        readAndWrite.writeWorkoutToUser(user.getWorkouts().get(0));
     }
 }
--- gr2303/src/main/java/core/User.java
+++ gr2303/src/main/java/core/User.java
@@ -1,36 +1,53 @@
 package core;
 
-//imports
 import java.util.ArrayList;
 
+/**
+ * This is the User class. It is used to store the users name and workouts.
+ */
 public class User {
-
-    //Fields
-    private String name;
+    /**
+     * This is the For the first version of the application we will only have one
+     * user.
+     * This is why we have a dummy name for the user. The idea is to have multiple
+     * users in the future.
+     */
+    private String name = "dummyName";
     private ArrayList<Workout> workouts = new ArrayList<Workout>();
 
-    //Constructor
-    public User(String name) {
-        this.name = name;
-        this.workouts = new ArrayList<Workout>();
-    }
-
-    //Getters
+    /**
+     * Returns the name of the user.
+     * 
+     * @return name in a String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the number of workouts the user has.
+     * 
+     * @return number of workouts in int
+     */
     public int getNumberOfWorkouts() {
         return workouts.size();
     }
 
+    /**
+     * Returns the workouts of the user.
+     * 
+     * @return workouts in an ArrayList
+     */
     public ArrayList<Workout> getWorkouts() {
         return workouts;
     }
 
-    //Logic
+    /**
+     * Adds a workout to the user.
+     * 
+     * @param workout Workout class
+     */
     public void addWorkout(Workout workout) {
         workouts.add(workout);
     }
-
 }
