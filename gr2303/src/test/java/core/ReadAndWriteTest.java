160f1efd070f1643f5c6c30f68cbc6bb5e89819c
Runar Saur Modahl <runarsm@stud.ntnu.no>
2023-09-20 21:15:26 +0200

    added some javadocs, format files, removed extra semicolons

--- gr2303/src/test/java/core/ReadAndWriteTest.java
+++ gr2303/src/test/java/core/ReadAndWriteTest.java
@@ -13,20 +13,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ReadAndWriteTest {
-    
+
     private ReadAndWrite readAndWrite;
 
-    private static String testFileLocation = System.getProperty("user.home") + System.getProperty("file.separator") + "testUserData.json";
+    private static String testFileLocation = System.getProperty("user.home") + System.getProperty("file.separator")
+            + "testUserData.json";
     private Random rand = new Random();
 
     private User user = new User();
-    
+
     private Workout workout1 = new Workout();
     private Workout workout2 = new Workout();
 
     private Exercise bench = new Exercise("Bench Press");
     private Exercise squat = new Exercise("Squats");
-    private Exercise deadlift = new Exercise("Deadlifts");; 
+    private Exercise deadlift = new Exercise("Deadlifts");
 
     private Set set1;
     private Set set2;
@@ -42,7 +43,7 @@ public class ReadAndWriteTest {
 
         bench.addSet(set1);
         bench.addSet(set2);
-        
+
         squat.addSet(set3);
         squat.addSet(set4);
 
@@ -67,6 +68,7 @@ public class ReadAndWriteTest {
         readAndWrite = new ReadAndWrite();
         assertInstanceOf(User.class, readAndWrite.returnUserClassFromFile(), "ReadAndWrite is not an instance of User");
     }
+
     @Test
     public void constructorTest2() {
         readAndWrite = new ReadAndWrite(testFileLocation);
@@ -80,8 +82,8 @@ public class ReadAndWriteTest {
         readAndWrite.writeWorkoutToUser(workout1);
         readAndWrite.writeWorkoutToUser(workout2);
         for (int i = 0; i < user.getNumberOfWorkouts(); i++) {
-            assertTrue(user.getWorkouts().get(i).equals(readAndWrite.returnUserClassFromFile().getWorkouts().get(i)), "Workouts are not equal");
-
+            assertTrue(user.getWorkouts().get(i).equals(readAndWrite.returnUserClassFromFile().getWorkouts().get(i)),
+                    "Workouts are not equal");
         }
         deleteTestfile();
     }
